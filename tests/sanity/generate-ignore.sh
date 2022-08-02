e4b5211b92839ec7068b1c7c326092a1ec773db7
Jadyn Emma Jaeger <j.jaeger@famedly.com>
2022-08-02 14:11:31 +0200

    feat(tests/sanity): Update ignore-2.x.txt

--- tests/sanity/generate-ignore.sh
+++ tests/sanity/generate-ignore.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd ../../
 while read -r line; do
-    find plugins/modules -name "*.py" | xargs -I {} -n 1 printf "{} $line\n"
+    find plugins -name "*.py" ! -path "plugins/module_utils/*" | xargs -I {} -n 1 printf "{} $line\n"
 done <"tests/sanity/ignore.template"
